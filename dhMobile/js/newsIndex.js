f71a9553b6f3345ebda92e73ba23ff24ee2effbf
SIRMly <429625819@qq.com>
2017-06-16 14:39:38 +0800

    ##

---
+++ dhMobile/js/newsIndex.js
@@ -0,0 +1,19 @@
+/**
+ * Created by SIRMly on 2017/6/16 0016.
+ */
+;(function ($){
+    document.addEventListener('touchmove', function (e) {
+        e.preventDefault();
+    });
+    var swiper = new Swiper('#swiper-top',{
+        simulateTouch: false,
+        slidesPerView: 1,
+        paginationClickable: false,
+        spaceBetween: 0,
+        loop: false,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+})(jQuery);
+
+
