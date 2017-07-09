7c9811731ec42aa33f42b35ad82978c2dae1f6a7
SIRMly <429625819@qq.com>
2017-07-09 20:37:48 +0800

    ##

--- js/newsIndex.js
+++ js/newsIndex.js
@@ -2,19 +2,30 @@
  * Created by SIRMly on 2017/6/16 0016.
  */
 ;(function ($){
-    //document.addEventListener('touchmove', function (e) {
-    //    e.preventDefault();
-    //});
     var swiper = new Swiper('#swiper-top',{
+        pagination: "#top-pagination",
         simulateTouch: false,
         slidesPerView: 1,
         paginationClickable: false,
         spaceBetween: 0,
         loop: true,
         autoplay: 2500,
+        //autoplay: false,
         autoplayDisableOnInteraction: false
     });
 
+    /*==鹿出现==*/
+    var winH = $(window).height();
+    var winW = $(window).width();
+    var deer = $(".deer");
+    var deerTop = deer.offset().top;
+    var container = $(".container");
+    container.scroll(function (){
+        deerTop = deer.offset().top;
+        if(deerTop <= (winH-0.2*winW)){
+            deer.addClass("deerShow");
+        }
+    });
 
 })(jQuery);
 
