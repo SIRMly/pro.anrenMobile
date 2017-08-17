15968c4633bd28dda16216add3e5a8b8b51e5ac0
SIRMly <429625819@qq.com>
2017-08-17 10:49:33 +0800

    many

--- js/newsIndex.js
+++ js/newsIndex.js
@@ -2,17 +2,6 @@
  * Created by SIRMly on 2017/6/16 0016.
  */
 ;(function ($){
-    var swiper = new Swiper('#swiper-top',{
-        pagination: "#top-pagination",
-        simulateTouch: false,
-        slidesPerView: 1,
-        paginationClickable: false,
-        spaceBetween: 0,
-        loop: true,
-        autoplay: 2500,
-        //autoplay: false,
-        autoplayDisableOnInteraction: false
-    });
 
     /*==鹿出现==*/
     var winH = $(window).height();
